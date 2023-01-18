f8c960e001f774e19f34a9bbca03c9412e745677
taneli1 <leevi.limnell@fi.experis.com>
2023-01-18 10:14:54 +0200

    01.18

---
+++ 01_18.js
@@ -0,0 +1,24 @@
+const MAP = new Map([
+  ["a", 1],
+  ["e", 2],
+  ["i", 3],
+  ["o", 4],
+  ["u", 5],
+])
+
+const replaceVowel = (string) => {
+  let str = string.toLowerCase()
+
+  for (const kv of MAP) {
+    str = str.replace(new RegExp(kv[0], "g"), kv[1])
+  }
+
+  return str
+}
+
+console.log(replaceVowel("karAchi"))
+console.log(replaceVowel("chEmBur"))
+console.log(replaceVowel("khandbari"))
+console.log(replaceVowel("LexiCAl"))
+console.log(replaceVowel("fuNctionS"))
+console.log(replaceVowel("EASY"))
