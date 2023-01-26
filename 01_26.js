b54531ae3382c6eae648b157fbc26e40ae915913
taneli1 <leevi.limnell@fi.experis.com>
2023-01-26 10:16:53 +0200

    26.01

---
+++ 01_26.js
@@ -0,0 +1,16 @@
+const happyAlgorithm = (num, arr = []) => {
+  if (arr.includes(num)) return `SAD ${arr.length}`
+
+  const total = [...num.toString()]
+    .map((str) => parseInt(str) * parseInt(str))
+    .reduce((a, b) => a + b)
+
+  if (total === 1) return `HAPPY ${arr.length + 1}`
+
+  return happyAlgorithm(total, [...arr, num])
+}
+
+console.log(happyAlgorithm(139))
+console.log(happyAlgorithm(67))
+console.log(happyAlgorithm(1))
+console.log(happyAlgorithm(89))
