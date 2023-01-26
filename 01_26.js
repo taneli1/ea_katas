83d6f6155534ee82beba7940b5bf2081acb56686
taneli1 <leevi.limnell@fi.experis.com>
2023-01-26 10:21:39 +0200

    26.01

--- 01_26.js
+++ 01_26.js
@@ -1,5 +1,5 @@
 const happyAlgorithm = (num, arr = []) => {
-  if (arr.includes(num)) return `SAD ${arr.length}`
+  if (arr.includes(num)) return `SAD ${arr.length - arr.indexOf(num)}`
 
   const total = [...num.toString()]
     .map((str) => parseInt(str) * parseInt(str))
